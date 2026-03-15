070276d8f0499c356d5a834b61a964f5d2780c95
jaime rey <jaimereycasado@gmail.com>
2026-03-15 15:00:37 +0100

    Accounts RateLimiter (151)

--- accounts/src/main/java/dev/jaimerey/accounts/controller/AccountsController.java
+++ accounts/src/main/java/dev/jaimerey/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import dev.jaimerey.accounts.dto.CustomerDto;
 import dev.jaimerey.accounts.dto.ErrorResponseDto;
 import dev.jaimerey.accounts.dto.ResponseDto;
 import dev.jaimerey.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -232,11 +233,19 @@ public class AccountsController {
                     )
             )
     })
+
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion(){
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 25");
     }
 
     @Operation(
